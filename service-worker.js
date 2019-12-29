0c78df89e0f90980cf12a5bfc52ddf244a25aad6
unknown <junaedsiam@gmail.com>
2019-12-30 02:32:51 +0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "junaedsiam.github.io/book-info-app-with-theme/precache-manifest.d7c327dac57448c611629311e1739c66.js"
+  "/book-info-with-theme/precache-manifest.318cab7f55c6a83ee04ceecfd2aebc74.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("junaedsiam.github.io/book-info-app-with-theme/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/book-info-with-theme/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
